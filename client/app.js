9218492b0d97a841d805d5fe9f8a4bb01f046afd
michaelost <mikhaylo.ostroviy@gmail.com>
2015-08-11 22:28:57 +0300

    first commit

---
+++ client/app.js
@@ -0,0 +1,119 @@
+Template.users.helpers({
+	'users' : function () {
+		return Meteor.users.find({});
+	}
+});
+
+
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password,
+            profile: {
+            	firstName: $('[name=firstName]').val(),
+            	lastName: $('[name=lastName]').val()
+            } 
+        }, function (error) {
+        	if(error) {
+        		console.log(error.reason);
+        	} else {
+        		var currentRoute = Router.current().route().getName();
+        		if (currentRoute == "login") {
+        			Router.go('/home');
+        		}
+        		
+        	}
+        });
+        Router.go('/home')
+    }
+});
+
+
+Template.navigation.events({
+	'click .logout' : function (event) {
+		event.preventDefault();
+		Meteor.logout();
+	}
+})
+
+
+Template.user.events({
+	'click .startDialog' : function (event) {
+		event.preventDefault();
+	 var currentUser = Meteor.userId();
+
+	var us =	Meteor.users.findOne({_id: currentUser});
+	 if (us.profile.dialogs) {
+	 	 Meteor.users.update(
+	 	{_id: currentUser}, 
+	 	{$push : {'profile.dialogs' : this._id}})
+	
+	
+
+	 } else {
+	 	 Meteor.users.update(
+				{_id: currentUser}, 
+	 			{$set : {'profile.dialogs' : [this._id]}});
+	
+	}
+	 
+	 console.log(us);
+	 console.log(this.profile.firstName);
+	}
+	
+})
+
+
+Template.login.events({
+	'submit form' : function (event) {
+		event.preventDefault();
+		var email = $("[name=email]").val();
+		var password = $('[name=password]').val();
+		Meteor.loginWithPassword(email,password, function (error) {
+			if (error) {
+				console.log(error.reason);
+			} else {
+				Router.go('/home');
+			}
+		});
+
+	}
+});
+
+
+Template.login.onRendered(function () {
+	$('form').validate();
+})
+
+Template.register.onRendered(function (){
+	$('form').validate();
+});
+
+
+
+$.validator.setDefault({
+	rules: {
+		email: {
+			required: true,
+			email: true
+		},
+		password: {
+			required: true,
+			minlength: 6
+		}
+	},
+	messages: {
+		email: {
+			required: "You must enter an email address",
+			email: "You`ve entered an invalid email address"
+		},
+		password: {
+			required: "You must enter a pssword",
+			minlength: "Your password must be at least{0} characters"
+		}
+	}
+});
---
+++ mApp.js
@@ -0,0 +1,24 @@
+Router.route('/users',{
+  template: 'users',
+  name: 'users'
+});
+
+Router.route('/register',{
+  template: 'register',
+  name: 'register'
+});
+Router.route('/login',{
+  template: 'login',
+  name: 'login'
+});
+
+Router.route('/home',{
+  template: 'home',
+  name: 'home'
+});
+
+
+
+Router.configure({
+    layoutTemplate: 'main'
+});
