f068993873c94bfa788aee0a700d24f6265fe391
michaelost <mikhaylo.ostroviy@gmail.com>
2015-08-12 14:47:39 +0300

    checking reciever dialogs

--- server/app.js
+++ server/app.js
@@ -2,13 +2,17 @@ Meteor.methods({
 	'sendMessage' : function (partnerID,message) {
 		console.log(Meteor.user());
 
-		
+		/* adding your message to current dialog with selected partner in your database */
 		Meteor.users.update({
 		'_id': Meteor.userId(),
 		'profile.dialogs' : { $elemMatch: { 'partnerId' : partnerID } }
 			},{
 		$push :{ "profile.dialogs.$.messages": message }})
-		
+		/* checking if reciever user already have dialog with you */
+			
+		 var resUser =  Meteor.users.findOne({'_id': partnerID });
 
+		 
+		  
 	}
 })
